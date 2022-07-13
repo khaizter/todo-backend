434dcc35503201147f77e245bf054ce29da40d92
khaizter <vashh.dev@gmail.com>
2022-07-13 23:13:44 +0800

    Minor changes for deployment test 1

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://todo-khaizter.netlify.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
