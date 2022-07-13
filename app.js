48eee46712d4a77e63d702207119029ca4345c1c
khaizter <vashh.dev@gmail.com>
2022-07-13 21:57:55 +0800

    Minor fix again on cors

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    process.env.CORS_WHITELIST || "http://localhost:3000"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
