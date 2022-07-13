136bb9442e18c9c550ceb55556b4813db4670e48
khaizter <vashh.dev@gmail.com>
2022-07-13 22:00:56 +0800

    Testing wildcard header

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
